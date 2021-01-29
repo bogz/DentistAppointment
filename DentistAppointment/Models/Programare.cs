9a51d0c947bd25fd32a8862aaa85a48de02433d3
Bogdan Orbai <bogz@archlinux.ro>
2021-01-29 13:49:09 +0200

    Programarile arata acum numele, prenumele, acabinetul si data.

--- DentistAppointment/Models/Programare.cs
+++ DentistAppointment/Models/Programare.cs
@@ -10,5 +10,28 @@ namespace DentistAppointment
         public int CabinetId { get; set; }
 
         public String Data { get; set; }
+
+        public String Client
+        {
+            get
+            {
+                using (var context = new Services.Context())
+                {
+                    Client obj = Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.FirstOrDefaultAsync(context.Clienti, c => c.ClientId == this.ClientId).Result;
+                    return obj.Nume + " " + obj.Prenume;
+                }
+            }
+        }
+        public String Cabinet
+        {
+            get
+            {
+                using (var context = new Services.Context())
+                {
+                    Cabinet obj = Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.FirstOrDefaultAsync(context.Cabinete, c => c.CabinetId == this.CabinetId).Result;
+                    return obj.Nume;
+                }
+            }
+        }
     }
 }
